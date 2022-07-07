d4a5ccae4192ec8afe727a87993716e9dfc4bef1
gencay.turan@student.pxl.be <gencay.turan@student.pxl.be>
2022-07-07 17:39:58 +0200

    home: added minimal layout & elements

--- Pokedex/src/app/home/home.component.ts
+++ Pokedex/src/app/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from '../pokemons';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  faFilter = faFilter;
+  pokemons = POKEMONS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
