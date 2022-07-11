46609aac73282794c383072477d5eba9a233aa6c
gencay.turan@student.pxl.be <gencay.turan@student.pxl.be>
2022-07-11 21:55:38 +0200

    routing update

--- Pokedex/src/app/app-routing.module.ts
+++ Pokedex/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { PokemonDetailComponent } from './features/pokemon-detail/pokemon-detail.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'pokemon-detail', component: PokemonDetailComponent },
 ];
 
 @NgModule({
