cbea3f33bdf361d5ee80ea48afe5038725a34d0f
gencay.turan@student.pxl.be <gencay.turan@student.pxl.be>
2022-07-11 21:55:01 +0200

    added responsiveService to components

--- Pokedex/src/app/features/home/home.component.ts
+++ Pokedex/src/app/features/home/home.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { POKEMONS } from '../../pokemons';
+import { Component, OnInit } from '@angular/core';
 import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { HomeSortMenuComponent } from '../home-sort-menu/home-sort-menu.component';
+import { ResponsiveService } from 'src/app/services/responsive.service';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +13,25 @@ export class HomeComponent implements OnInit {
   pokemons: any;
   faFilter = faFilter;
 
-  constructor(private matBottomSheet: MatBottomSheet) {}
+  isSmall: boolean;
+  isXSmall: boolean;
+  isMobile: boolean;
+
+  constructor(
+    private matBottomSheet: MatBottomSheet,
+    private responsiveService: ResponsiveService
+  ) {
+    // responsiveService Subscriptions
+    this.responsiveService.SmallChanged.subscribe((isSmall) => {
+      this.isSmall = isSmall;
+    });
+    this.responsiveService.XSmallChanged.subscribe((isXSmall) => {
+      this.isXSmall = isXSmall;
+    });
+    this.responsiveService.viewMobileChanged.subscribe((mobileView) => {
+      this.isMobile = mobileView;
+    });
+  }
 
   openBottomSheet() {
     let bottomSheet = this.matBottomSheet.open(HomeSortMenuComponent, {
@@ -27,5 +45,7 @@ export class HomeComponent implements OnInit {
 
   //after clicking sort option, dismiss sheet and trigger selected function.
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.isMobile);
+  }
 }
--- Pokedex/src/app/features/pokemon-detail/pokemon-detail.component.ts
+++ Pokedex/src/app/features/pokemon-detail/pokemon-detail.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ResponsiveService } from 'src/app/services/responsive.service';
 
 @Component({
   selector: 'app-pokemon-detail',
   templateUrl: './pokemon-detail.component.html',
-  styleUrls: ['./pokemon-detail.component.css']
+  styleUrls: ['./pokemon-detail.component.css'],
 })
 export class PokemonDetailComponent implements OnInit {
+  pokemons: any;
 
-  constructor() { }
+  isSmall: boolean;
+  isXSmall: boolean;
+  isMobile: boolean;
 
-  ngOnInit(): void {
+  constructor(private responsiveService: ResponsiveService) {
+    // responsiveService Subscriptions
+    this.responsiveService.SmallChanged.subscribe((isSmall) => {
+      this.isSmall = isSmall;
+    });
+    this.responsiveService.XSmallChanged.subscribe((isXSmall) => {
+      this.isXSmall = isXSmall;
+    });
+    this.responsiveService.viewMobileChanged.subscribe((mobileView) => {
+      this.isMobile = mobileView;
+    });
   }
 
+  ngOnInit(): void {}
+
+  receivePokemons($event: any) {
+    this.pokemons = $event;
+  }
 }
